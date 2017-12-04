861ec3915a9bf95fc9f6f2a7cb9a32f9f2741b82
Junior Gantin <nioperas06@gmail.com>
2017-12-04 13:15:29 +0100

    Add authentication tests

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,83 +1,72 @@
-# import json
-#
-# from django.contrib.auth.models import User
-# from django.core.urlresolvers import reverse
-#
-# from rest_framework.authtoken.models import Token
-# from rest_framework.test import APITestCase
-#
-#
-# class UserRegistrationAPIViewTestCase(APITestCase):
-#     url = reverse("users:list")
-#
-#     def test_invalid_password(self):
-#         """
-#         Test to verify that a post call with invalid passwords
-#         """
-#         user_data = {
-#             "username": "testuser",
-#             "email": "test@testuser.com",
-#             "password": "password",
-#             "confirm_password": "INVALID_PASSWORD"
-#         }
-#         response = self.client.post(self.url, user_data)
-#         self.assertEqual(400, response.status_code)
-#
-#     def test_user_registration(self):
-#         """
-#         Test to verify that a post call with user valid data
-#         """
-#         user_data = {
-#             "username": "testuser",
-#             "email": "test@testuser.com",
-#             "password": "123123",
-#             "confirm_password": "123123"
-#         }
-#         response = self.client.post(self.url, user_data)
-#         self.assertEqual(201, response.status_code)
-#         self.assertTrue("token" in json.loads(response.content))
-#
-#     def test_unique_username_validation(self):
-#         """
-#         Test to verify that a post call with already exists username
-#         """
-#         user_data_1 = {
-#             "username": "testuser",
-#             "email": "test@testuser.com",
-#             "password": "123123",
-#             "confirm_password": "123123"
-#         }
-#         response = self.client.post(self.url, user_data_1)
-#         self.assertEqual(201, response.status_code)
-#
-#         user_data_2 = {
-#             "username": "testuser",
-#             "email": "test2@testuser.com",
-#             "password": "123123",
-#             "confirm_password": "123123"
-#         }
-#         response = self.client.post(self.url, user_data_2)
-#         self.assertEqual(400, response.status_code)
-#
-#
-# class UserLoginAPIViewTestCase(APITestCase):
-#     url = reverse("users:login")
-#
-#     def setUp(self):
-#         self.username = "john"
-#         self.email = "john@snow.com"
-#         self.password = "you_know_nothing"
-#         self.user = User.objects.create_user(self.username, self.email, self.password)
-#
-#     def test_authentication_without_password(self):
-#         response = self.client.post(self.url, {"username": "snowman"})
-#         self.assertEqual(400, response.status_code)
-#
-#     def test_authentication_with_wrong_password(self):
-#         response = self.client.post(self.url, {"username": self.username, "password": "I_know"})
-#         self.assertEqual(400, response.status_code)
-#
-#     def test_authentication_with_valid_data(self):
-#         response = self.client.post(self.url, {"username": self.username, "password": self.password})
-#         self.assertEqual(200, response.status_code)
-#         self.assertTrue("auth_token" in json.loads(response.content))
+import json
+from authentication.models import User
+from authentication.managers import UserManager
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+class UserRegistrationAPIViewTestCase(APITestCase):
+
+    def test_user_registration(self):
+        """
+        Test to verify that a post call with user valid data
+        """
+        user_data = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "test@testuser.com",
+            "password": "yetanotherpassword",
+            "user_type": "CANDIDATE"
+        }
+        response = self.client.post('http://127.0.0.1:8000/v1/auth/register/', user_data)
+        self.assertEqual(201, response.status_code)
+
+    def test_unique_username_validation(self):
+        """
+        Test to verify that a post call with already exists username
+        """
+        user_data_1 = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "test@testuser.com",
+            "password": "yetanotherpassword",
+            "user_type": "CANDIDATE"
+        }
+        response = self.client.post('http://127.0.0.1:8000/v1/auth/register/', user_data_1)
+        self.assertEqual(201, response.status_code)
+
+        user_data_2 = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "test@testuser.com",
+            "password": "yetanotherpassword",
+            "user_type": "COMPANY"
+        }
+        response = self.client.post('http://127.0.0.1:8000/v1/auth/register/', user_data_2)
+        self.assertEqual(400, response.status_code)
+
+
+class UserLoginAPIViewTestCase(APITestCase):
+    url = 'http://127.0.0.1:8000/v1/auth/login/'
+
+    def setUp(self):
+        user_data = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "test@testuser.com",
+            "password": "yetanotherpassword",
+            "user_type": "COMPANY"
+        }
+        self.client.post('http://127.0.0.1:8000/v1/auth/register/', user_data)
+
+    def test_authentication_without_password(self):
+        response = self.client.post(self.url, {"username": "snowman"})
+        self.assertEqual(400, response.status_code)
+
+    def test_authentication_with_wrong_password(self):
+        response = self.client.post(self.url, {"email": "test@testuser.com", "password": "I_know"})
+        self.assertEqual(400, response.status_code)
+
+    def test_authentication_with_valid_data(self):
+        response = self.client.post(self.url, {"email": "test@testuser.com", "password": "yetanotherpassword"})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("token" in json.loads(response.content))
