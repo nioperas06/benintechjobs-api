6b425195e737cd1f537d375c2763f18b793922eb
Junior Gantin <nioperas06@gmail.com>
2018-03-29 10:29:32 +0100

    Fix STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

--- benintechjobs/settings.py
+++ benintechjobs/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'api',
     'django.contrib.admin',
+    'whitenoise'
 ]
 
 MIDDLEWARE = [
@@ -128,9 +129,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
