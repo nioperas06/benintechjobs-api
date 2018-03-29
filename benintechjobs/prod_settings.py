35a2f8399c317222695c1e5160749a0471b3f2bd
Junior Gantin <nioperas06@gmail.com>
2018-03-29 11:01:22 +0100

    Add dj-database-url && pyscopg2

--- benintechjobs/prod_settings.py
+++ benintechjobs/prod_settings.py
@@ -1,7 +1,10 @@
 from .settings import *
+import dj_database_url
 
 ALLOWED_HOSTS += ['benintechjobs-api.herokuapp.com']
 
 DEBUG = False
 
-SECRET_KEY = 'xxl!$adm6exadw#7l$2m0!ek@7h*v3v#4n-t(k(vlzubxjiduv'
+SECRET_KEY = '$jz&q54#+1kxfo927x_u-o*2qmqk=3@&n(arz!m4#u0dy&c=z0'
+
+DATABASES['default'] = dj_database_url.config('postgres://spzkaskyeltuxx:f97d829558c33071384a873a1df772de0ea2d55356a64da5f976e4aeecbb83c5@ec2-23-21-121-220.compute-1.amazonaws.com:5432/d24gsbaho305ck')
