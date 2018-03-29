2c0e92f53d3d4d95010661b80250b600b7c8e2c2
Junior Gantin <nioperas06@gmail.com>
2018-03-29 23:48:35 +0100

    Prod settings

--- benintechjobs/prod_settings.py
+++ benintechjobs/prod_settings.py
@@ -7,4 +7,4 @@ DEBUG = False
 
 SECRET_KEY = '$jz&q54#+1kxfo927x_u-o*2qmqk=3@&n(arz!m4#u0dy&c=z0'
 
-DATABASES['default'] = dj_database_url.config(default='postgres://ueuxlbahahfzde:d834461fb36d394d713ae5e973f85d68035dd078e7a731e0ae62331de0554be6@ec2-54-235-109-37.compute-1.amazonaws.com:5432/d80ft02v463v9l')
+DATABASES['default'] = dj_database_url.config(default='postgres://oclwlticjzoqnt:1394f86a64e63aef27b9c3768b0957af952ee5ff120ef1d047103b1d6d18b827@ec2-23-21-121-220.compute-1.amazonaws.com:5432/datvsjjaf1bp76')
