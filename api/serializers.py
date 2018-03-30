d2a43352203a975f13467bdb9b3a9e16492ffc69
Junior Gantin <nioperas06@gmail.com>
2018-03-30 02:40:03 +0100

    MVP 0.1

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ('id', 'company', 'email', 'url', 'title', 'location', 'description', 'logo')
+        fields = ('id', 'company', 'email', 'url', 'title', 'location', 'description', 'logo', 'created_at')
